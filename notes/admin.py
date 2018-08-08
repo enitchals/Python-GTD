0e744955dca694a680317c27b18c92c0dca01a8e
Ellen Nitchals <ellen.nitchals@gmail.com>
2018-08-08 14:34:44 -0700

    some progress

--- notes/admin.py
+++ notes/admin.py
@@ -3,3 +3,6 @@ from django.contrib import admin
 # Register your models here.
 from .models import Note
 admin.site.register(Note)
+
+from .models import PersonalNote
+admin.site.register(PersonalNote)
